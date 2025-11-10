0d70844fbc1fc016bbb682148b01af9d3cd747b5
mike_the_sloth <mjooste13@gmail.com>
2025-11-10 08:28:13 +0200

    to_html methods

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,2 +1,64 @@
 class HTMLNode:
-    pass
+    def __init__(self,tag = None,value = None,children = None,props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+
+    def props_to_html(self):
+        if self.props == None:
+            return ""
+        prop_list = []
+        for key in self.props.keys():
+            prop_list.append(f' {key}="{self.props[key]}"')
+        return "".join(prop_list)
+    
+    def __repr__(self):
+        return f'HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})'
+
+class LeafNode(HTMLNode):
+    def __init__(self,tag,value,props=None):
+        super(LeafNode, self).__init__(tag,value,None,props)
+    def to_html(self):
+        if not self.value or self.value == None:
+            raise ValueError("Leaf nodes need a value")
+        if self.tag == None:
+            return self.value
+        else:
+            return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self,tag,children,props=None):
+        super(ParentNode, self).__init__(tag,None,children,props)
+    def to_html(self):
+        if not self.tag or self.tag == None:
+            raise ValueError("Parent nodes need a tag")
+        if self.children == None:
+            raise ValueError("Parent nodes need a child")
+        else:
+            html = f'<{self.tag}{self.props_to_html()}>'
+            for child in self.children:
+                html += child.to_html()
+            html += f'</{self.tag}>'
+            return html
+    
+    def __repr__(self):
+        return f"parent({self.tag}, {self.children}, {self.props})"
+
+'''
+An HTMLNode without a tag will just render as raw text
+An HTMLNode without a value will be assumed to have children
+An HTMLNode without children will be assumed to have a value
+An HTMLNode without props simply won't have any attributes
+'''
+#tag - HTML tag name (e.g. "p", "a", "h1", etc.)
+#value - (e.g. the text inside a paragraph)
+#children - A list of HTMLNode children of this node
+#props - dictionary of key-value pairs: attributes of HTML tag. 
+#       eg link (<a> tag) might have {"href": "https://www.google.com"}
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -58,7 +58,32 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("Jiggly",TextType.TEXT,'www.google.com')
         node2 = TextNode("Jiggly",TextType.TEXT,'www.yahoo.com')
         self.assertNotEqual(node,node2)
+    
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.TEXT)
+        self.assertEqual(node, node2)
 
+    def test_eq_false(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        node2 = TextNode("This is a text node2", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.ITALIC, "https://www.boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
 
 if __name__ == "__main__":
     unittest.main()
